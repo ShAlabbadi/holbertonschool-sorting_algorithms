d09767b60b5052edc6ed4db3265b3437288f5421
ShAlabbadi <10708@holbertonstudents.com>
2025-04-09 08:44:37 +0000

    12. Dealer

--- deck.h
+++ deck.h
@@ -1,13 +1,19 @@
 #ifndef DECK_H
 #define DECK_H
 
-
+/**
+ * enum kind_e - Represents the four standard playing card suits
+ * @SPADE: Spades suit
+ * @HEART: Hearts suit
+ * @CLUB: Clubs suit
+ * @DIAMOND: Diamonds suit
+ */
 typedef enum kind_e
 {
-    SPADE = 0,
-    HEART,
-    CLUB,
-    DIAMOND
+	SPADE = 0,
+	HEART,
+	CLUB,
+	DIAMOND
 } kind_t;
 
 /**
@@ -19,8 +25,8 @@ typedef enum kind_e
  */
 typedef struct card_s
 {
-    const char *value;
-    const kind_t kind;
+	const char *value;
+	const kind_t kind;
 } card_t;
 
 /**
@@ -32,13 +38,13 @@ typedef struct card_s
  */
 typedef struct deck_node_s
 {
-    const card_t *card;
-    struct deck_node_s *prev;
-    struct deck_node_s *next;
+	const card_t *card;
+	struct deck_node_s *prev;
+	struct deck_node_s *next;
 } deck_node_t;
 
 
 void sort_deck(deck_node_t **deck);
 
 
-#endif 
+#endif
